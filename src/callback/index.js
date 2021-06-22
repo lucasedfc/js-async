f310bda42fada19b296c03990da02d0f4bb9f12d
Lucas Heim <lucasedfc@gmail.com>
2021-06-22 20:51:04 -0300

    CallBacks

---
+++ src/callback/index.js
@@ -0,0 +1,23 @@
+/**CALLBACK */
+function sum(num1, num2) {
+  return num1 + num2;
+}
+
+function calc(num1, num2, callback) {
+  return callback(num1, num2);
+}
+
+console.log(calc(6, 2, sum));
+
+function date(callback) {
+  console.log("date", new Date());
+  setTimeout(() => {
+    let date = new Date();
+    callback(date);
+  }, 3000);
+}
+
+function printDate(dateNow) {
+  console.log("print", dateNow);
+}
+date(printDate);
